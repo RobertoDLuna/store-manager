af4da10aff12ecec7ceafe21b923001928865bde
Roberto Luna <robertocgw@gmail.com>
2022-10-04 15:21:50 -0300

    criação da rota de sales

---
+++ src/routes/sales.router.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const salesController = require('../controllers/sales.controller');
+
+const salesRouter = express.Router();
+
+salesRouter.post('/', salesController.addSales);
