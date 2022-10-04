018eaf34ac4f3ae4d09e78b52f4ee1e89599c534
Roberto Luna <robertocgw@gmail.com>
2022-10-04 14:01:43 -0300

    feat: req4 - Crie validações para produtos

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
+const { validationName } = require('../middlewares/validationMiddleware');
 
 const productsRouter = express.Router();
 
 productsRouter.get('/', productsController.listProducts);
 productsRouter.get('/:id', productsController.listProductsById);
-productsRouter.post('/', productsController.addProduct);
+productsRouter.post('/', validationName, productsController.addProduct);
 
 module.exports = productsRouter;
